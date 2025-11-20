1fd05660a78017be0c27f303036d3207108061ad
Kocsis Gergely <kocsisger@example.com>
2025-11-20 13:19:15 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs122/BootstrapCs122Application.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapCs122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapCs122Application.class, args);
+    }
+
+}
---
+++ src/test/java/hu/unideb/inf/bootstrap_cs122/BootstrapCs122ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapCs122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
