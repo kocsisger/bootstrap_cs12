339dc053280bb5467a95d42c0711661bc665cd40
Kocsis Gergely <kocsisger@example.com>
2025-11-27 13:08:16 +0100

    C R

--- src/main/java/hu/unideb/inf/bootstrap_cs122/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap_cs122/controller/MainController.java
@@ -1,10 +1,17 @@
 package hu.unideb.inf.bootstrap_cs122.controller;
 
+import hu.unideb.inf.bootstrap_cs122.model.Person;
+import hu.unideb.inf.bootstrap_cs122.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    PersonRepository personRepository;
 
     @GetMapping("")
     public String showHomePage() {
@@ -12,8 +19,21 @@ public class MainController {
     }
 
     @GetMapping("/persons")
-    public String showPersons() {
+    public String showPersons(Model model) {
+        model.addAttribute("personsList", personRepository.findAll());
         return "persons";
+    }
+
+    @GetMapping("/persons/new")
+    public String newPerson(Model model) {
+        model.addAttribute("newPerson", new Person());
+        return "newPersonForm";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person newPerson) {
+        personRepository.save(newPerson);
+        return "redirect:/persons";
     }
 
 }
